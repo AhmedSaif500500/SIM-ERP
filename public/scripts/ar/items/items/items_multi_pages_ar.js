af3a4d99f226ed2dfc5d4b5a9448ea62cb0a685b
AhmedSaif500500 <152290867+AhmedSaif500500@users.noreply.github.com>
2024-12-25 02:39:20 -0800

    q

--- public/scripts/ar/items/items/items_multi_pages_ar.js
+++
@@ -1,7 +0,0 @@
-
-function clear_items_sessionsStorage(){
-    sessionStorage.removeItem('obj_items_create_group');
-    sessionStorage.removeItem('obj_items_create_account');
-    sessionStorage.removeItem('obj_items_update_account');
-    sessionStorage.removeItem('obj_items_update_group');
-}
--- public/scripts/ar/test.js
+++
@@ -1,9 +0,0 @@
-const dropdown_div = document.querySelector(`#dropdown_div`)
-document.addEventListener('DOMContentLoaded', function() {
-  showLoadingIcon(content_space)
-  create_drop_down(`dropdown_div`,'/getEmployeesData1','effects_permission','view')
-  create_drop_down(`dropdown_div2`,'/getEmployeesData1','effects_permission','view')
- hideLoadingIcon(content_space)
-});
-
-
